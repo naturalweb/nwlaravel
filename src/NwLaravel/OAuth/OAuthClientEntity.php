460a3566c4c07696942d4c282c3116cab369173f
Renato Moura <renato@naturalweb.com.br>
2016-08-28 07:21:31 -0300

    Add documentation in OAuthClientEntity

--- src/NwLaravel/OAuth/OAuthClientEntity.php
+++ src/NwLaravel/OAuth/OAuthClientEntity.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 /**
  * Class OAuthClient Entity
+ *
+ * @method OAuthClientEntity where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method Eloquent first($columns = ['*'])
  */
 class OAuthClientEntity extends Eloquent
 {
